13954c1b37a1eb408a58e926ae454b7f4324ad2a
Fahad98723 <md.ravi63@gmail.com>
2023-06-11 00:35:47 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 async function main() {
   try {
-    await mongoose.connect(config.datebase_url as string);
+    await mongoose.connect(config.datebase_url as string)
 
-    console.log(`Database is conneted succesfully`);
+               console.log(`Database is conneted succesfully`)
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(`Failed to connnect database`, error);
+    console.log(`Failed to connnect database`, error)
   }
 }
 
-main().catch((err) => console.log(err));
+main().catch(err => console.log(err))
